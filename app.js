95ec6953316e0e76943f5b81f94a892db5ae69d0
niksein007 <markpearceniksein@gmail.com>
2020-02-21 16:15:52 +0000

    first update

---
+++ app.js
@@ -0,0 +1,27 @@
+class Card{
+    constructor(name,picture){
+        _name = name;
+        _pic = pic;
+    }
+
+get name(){
+    return this._name;
+}
+
+get picture(){
+    return this._picture;
+}
+
+}
+
+class Player{
+
+}
+
+class Topic{
+
+}
+
+class Character extends Card{
+
+}
